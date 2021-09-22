e314d11e66d8059ee2157e9f1da80e401522f7a7
Claudia Comito <c.comito@fz-juelich.de>
2021-09-22 10:16:36 +0200

    Debugging deadlock

--- elephant/asset/asset.py
+++ elephant/asset/asset.py
@@ -1679,8 +1679,8 @@ def _pmat_neighbors_ht(mat, filter_shape, n_largest):
             t_lmat = t_lmat[:, l//2:-l//2, :]
 #        mat.comm.Barrier()
     # wrap local t_lmat into global lmat (imbalanced because of calc over halos)
-    lmat = ht.dndarray.DNDarray(t_lmat, gshape=(n_largest,) + mat.shape, dtype=ht.float32, split=1, device=mat.device, comm=mat.comm, balanced=False)
-    #lmat = ht.array(t_lmat, is_split=1, device=mat.device)
+    #lmat = ht.dndarray.DNDarray(t_lmat, gshape=(n_largest,) + mat.shape, dtype=ht.float32, split=1, device=mat.device, comm=mat.comm, balanced=False)
+    lmat = ht.array(t_lmat, is_split=1, device=mat.device)
     lmat.balance_()
     return lmat
 
