55e7b949b6fb19d6ca25b71b554b616f16ad659c
Claudia Comito <c.comito@fz-juelich.de>
2022-01-12 06:08:35 +0100

    Timing

--- elephant/asset/asset.py
+++ elephant/asset/asset.py
@@ -2900,9 +2900,9 @@ class ASSET(object):
         np_pmat = scipy.stats.poisson.cdf(imat.larray.numpy() - 1, Mu.larray.numpy())
         if imat.comm.rank == 0:
             log.warning("PMAT: np_mat calculated")
-        start = time.perf_counter()
+        start = time.time()
         pmat = ht.array(np_pmat, is_split=0, force_check=True)#, copy=False) # TODO: WHY DOES COPY=FALSE REQUIRE MORE MEMORY HERE?
-        end = time.perf_counter()
+        end = time.time()
         if imat.comm.rank == 0:
             log.warning("PMAT: distributed pmat: took {} seconds".format(end - start))
 
